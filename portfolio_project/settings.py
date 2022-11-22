f3c9f93696d4d039b946be6b97710fabaa312f48
mjayb <maryjaneamarachi04@gmail.com>
2022-11-22 16:07:25 +0100

    contact form

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -160,6 +160,12 @@ EMAIL_HOST_PASSWORD =email_host_password
 # To email: contact form
 RECIPIENT_ADDRESS=recipient_address
 
-CORS_ALLOWED_ORIGINS=True
+CORS_ALLOWED_ORIGINS=[
+    "http://localhost:8080",
+    "https://mj-portfolio-production.up.railway.app"
 
 
+]
+
+
+CSRF_TRUSTED_ORIGINS=["https://mj-portfolio-production.up.railway.app"]
